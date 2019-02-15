abfb0619635ded0fcfa68e4feedb46cfb1f0adf6
Yurii Slynko <ummbriaco@gmail.com>
2019-02-15 12:48:09 +0200

    Lesson 4

--- src/test/java/LoginTests.java
+++ src/test/java/LoginTests.java
@@ -3,40 +3,47 @@ import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 public class LoginTests {
 
+    WebDriver driver;
+
+    @BeforeMethod
+    public void beforeMethod(){
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\slynko_y\\IdeaProjects\\qaauto-5.02.2019\\chromedriver.exe");
+        driver = new ChromeDriver();
+        driver.get("https://www.linkedin.com/");
+    }
+
+    @AfterMethod
+    public void afterMethod(){
+        driver.quit();
+    }
+
     @Test
     public void successfulLoginTest() {
-        System.setProperty("webdriver.chrome.driver", "C:\\Users\\slynko_y\\IdeaProjects\\qaauto-5.02.2019\\chromedriver.exe");
-        WebDriver driver = new ChromeDriver();
-        driver.get("https://www.linkedin.com/");
 
-        String loginEmail = "baltimore.1729@meta.ua";
-        String loginPassword = "QWErty_10%";
+        WebElement userEmailField = driver.findElement(By.xpath("//input[@id='login-email']"));
+        WebElement userPasswordField = driver.findElement(By.xpath("//input[@id='login-password']"));
+        WebElement signInButton = driver.findElement(By.xpath("//input[@id='login-submit']"));
 
-        WebElement searchField;
-        searchField = driver.findElement(By.xpath("//input[@id='login-email']"));
-        searchField.sendKeys(loginEmail);//Login field filling
-        searchField = driver.findElement(By.xpath("//input[@id='login-password']"));
-        searchField.sendKeys(loginPassword);//Password field filling
-        searchField = driver.findElement(By.xpath("//input[@id='login-submit']"));
-        searchField.click();//Submit credentials
+        userEmailField.sendKeys("ludalm41@i.ua");
+        userPasswordField.sendKeys("e1dk99");
+        signInButton.click();
 
-        searchField = driver.findElement(By.xpath("//span[.='Профиль'][contains(@class, 'nav-item__dropdown')]"));
-        searchField.click();
+        WebElement profileMenuItem = driver.findElement(By.xpath("//li[@id='profile-nav-item']"));
 
-        searchField = driver.findElement(By.xpath("//span[contains(@class, 'linkcard-link')]"));
-        searchField.click();
-        //extra_check
-        if (driver.findElements(By.xpath("//button[@data-control-name='add_start_date']")).size() != 0)
-        {
-            System.out.println("PASSED");
-        }
-            else
-        {
-            System.out.println("FAILED");
-        }
+        profileMenuItem.isDisplayed();
+        Assert.assertTrue(profileMenuItem.isDisplayed(), "profileMenuItem is not displayed on Home page.");
+        Assert.assertEquals(driver.getCurrentUrl(), "https://www.linkedin.com/feed/", "Home page URL is not correct");
+    }
+
+    @Test
+    public void negativeLoginTest(){
+
     }
 }
